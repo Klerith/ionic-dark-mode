8cff04c1d320a8ec80d732420c20bc66e30972a5
Fernando Herrera <fernando.herrera85@gmail.com>
2019-11-05 19:36:27 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,16 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.checkDarkTheme();
     });
   }
+
+  checkDarkTheme() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    if ( prefersDark.matches ) {
+      document.body.classList.toggle( 'dark' );
+    }
+  }
+
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  darkMode: boolean = true;
+
+  constructor() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.darkMode = prefersDark.matches;
+  }
+
+
+  cambio() {
+    // const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle( 'dark' );
+    
+  }
+
 
 }
